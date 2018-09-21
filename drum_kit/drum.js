4aaf70d87136568b95eb3b566fc795039c4d20f9
Nosson <nossonhuebner@gmail.com>
2018-09-21 00:10:25 -0400

    Add some styling

--- drum_kit/drum.js
+++ drum_kit/drum.js
@@ -1,6 +1,18 @@
 window.addEventListener("keydown", (e) => {
   const audio = document.querySelector(`audio[data-key=${e.key}]`);
-  if (audio) {
-    audio.play();
-  }
+  const key = document.querySelector(`.key[data-key=${e.key}]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
+  // key.classList.remove('playing');
+
+  audio.currentTime = 0;
+  audio.play();
 });
+
+const removeTransition = (e) => {
+  console.log(e);
+};
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
